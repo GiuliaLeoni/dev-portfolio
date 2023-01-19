e46c8284357077eb2520f12ff2043e2b9fe2606f
GiuliaLeoni <leonig255@gmail.com>
2023-01-19 09:56:34 +0000

    reformat header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,13 +4,14 @@ import headerStyles from './header.module.scss';
 export const Header = () => {
     return (
         <>
-            <section className={headerStyles.container}>
-                <h1 className={headerStyles.name}>Giulia Leoni</h1>
-                <section className={headerStyles.btns}>
-                    <button><span>mode</span></button>
-                    <button><span>party</span></button>
+        {/* V1 */}
+            <main className={headerStyles.main}>
+                <section className={headerStyles.container}>
+                    <h3 className={headerStyles.text}>GIULIA LEONI</h3>
+                    <h3 className={headerStyles.text}>CONTACT</h3>
                 </section>
-            </section>
+            </main>
+
             <Outlet />
         </>
     )
