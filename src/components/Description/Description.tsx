3b2eafa2f7ea3e25e98f9418fd17be7fc84e62a6
GiuliaLeoni <leonig255@gmail.com>
2023-02-02 12:24:12 +0000

    resizing font awesome icon

--- src/components/Description/Description.tsx
+++ src/components/Description/Description.tsx
@@ -4,14 +4,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
 export const Description = () => {
+
+    
+
     return (
         <>
             <section>
 
                 <section className={descrStyles.title}>
                     <h1 className={descrStyles.titleText}>creative developer</h1>
-                   <FontAwesomeIcon icon={faRainbow} size="2x"
-                   style={{paddingTop: '0.8rem'}}/>
+                   <FontAwesomeIcon className={descrStyles.icon} icon={faRainbow} size="2x" bounce
+                   style={{paddingTop: '1.3rem'}}/>
                 </section>
         
                    <h4>who finds joy in the challenge of creating for people</h4>
