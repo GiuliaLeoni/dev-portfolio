c818e9d780368e98edb0aaceabc2b27689899b25
GiuliaLeoni <leonig255@gmail.com>
2023-02-02 12:01:58 +0000

    add font awesome icon

--- src/components/Description/Description.tsx
+++ src/components/Description/Description.tsx
@@ -1,10 +1,19 @@
 import descrStyles from './description.module.scss';
+import { faRainbow } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 export const Description = () => {
     return (
         <>
             <section>
-                   <h1 className={descrStyles.title}>creative developer</h1>
+
+                <section className={descrStyles.title}>
+                    <h1 className={descrStyles.titleText}>creative developer</h1>
+                   <FontAwesomeIcon icon={faRainbow} size="2x"
+                   style={{paddingTop: '0.8rem'}}/>
+                </section>
+        
                    <h4>who finds joy in the challenge of creating for people</h4>
                    <p>Graduate designer - currently Full-Stack Designer Intern @ Autone
 Interested in interaction and designing and building outstanding user experiences</p>
