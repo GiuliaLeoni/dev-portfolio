9cabd3950e9a1bd16ec0d149783675f2ee5613aa
GiuliaLeoni <leonig255@gmail.com>
2023-02-02 13:53:45 +0000

    insert icons

--- src/components/Links/Links.tsx
+++ src/components/Links/Links.tsx
@@ -1,5 +1,13 @@
-import { Link } from 'react-router-dom'
-import linksStyles from './links.module.scss'
+import { Link } from 'react-router-dom';
+import linksStyles from './links.module.scss';
+
+// import linkedin from '../../assets/icons/linkedin.svg';
+// import github from '../../assets/icons/github.svg';
+// import email from '../../assets/icons/email.svg';
+// import cv from '../../assets/icons/cv.svg';
+
+import { LinkedIn, Github, Email, Cv, Arrow } from '../Icons';
+
 
 export const Links = () => {
     return (
@@ -8,11 +16,12 @@ export const Links = () => {
                 
                 <section>
                     <h3 className={linksStyles.projects}>projects</h3>
-                    <span>arrow</span>
+                    {/* <span>arrow</span> */}
+                    <Arrow />
                 </section>
 
 
-                <section className={linksStyles.links}>
+                {/* <section className={linksStyles.links}>
                     <section className={linksStyles.link}>   
                         <Link to='/about'>about me</Link>
                     </section>
@@ -25,6 +34,14 @@ export const Links = () => {
                     <section className={linksStyles.link}>
                         <a href="/">leonig255@gmail.com</a>
                     </section>
+                </section> */}
+
+                <section className={linksStyles.links}>
+                    {/* <img src="linkedin" alt="linkedin"/> */}
+                    <LinkedIn />
+                    <Github />
+                    <Cv />
+                    <Email />
                 </section>
 
             </section>
